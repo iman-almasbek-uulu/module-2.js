ef72e4ebfdd9bf2671ad7893140fae27746f46f5
Iman Almasbek uulu <iman.almasbekuulu.kk@gmail.com>
2022-06-03 12:44:13 +0600

    exercise-3

---
+++ exercise-3/js/script-7.js
@@ -0,0 +1,9 @@
+function even_or_odd(number) {
+    let res;
+    if (number % 2 === 0 ) {
+      res = 'Even';
+    }else {
+      res = "Odd"
+    }
+    return res;
+  }
---
+++ exercise-3/js/script-8.js
@@ -0,0 +1,5 @@
+function areaLargestSquare(r) {
+    return (r ** 2) * 2;;
+  }
+  
+  areaLargestSquare(2);
